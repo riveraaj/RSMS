37f8b1071741420177476635a349f8e191a4d93b
riveraaj <jonathandavidr7@gmail.com>
2025-03-16 14:27:14 -0600

    (WIP)[FEATURE] Create the Business Objects project in the Application Businness Rules layer and create the outputport interface.

---
+++ RSMS.App/RSMS.BusinessObjects/Interfaces/Ports/IOutputPort.cs
@@ -0,0 +1,7 @@
+﻿namespace RSMS.BusinessObjects.Interfaces.Ports;
+
+public interface IOutputPort
+{
+    IOperationResponse OperationResponse { get; }
+    Task Handle(IOperationResponse oOperationResponse);
+}
