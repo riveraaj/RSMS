2c0d10c680cab7a5779f0b778083b129b0f3454a
riveraaj <jonathandavidr7@gmail.com>
2025-03-16 22:33:58 -0600

    [FEATURE] Implement Invertion of Control

---
+++ RSMS.App/RSMS.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace RSMS.IoC;
+public static class DependencyContainer
+{
+    public static IServiceCollection AddRSMSServices(this IServiceCollection services,
+                                                          IConfiguration configuration)
+    {
+        services.AddBusinessLogicServices()
+                .AddRepositories()
+                .AddDataContextServices(configuration)
+                .AddPresenterServices();
+
+        return services;
+    }
+}
---
+++ RSMS.App/RSMS.IoC/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using RSMS.BusinessLogic;
+global using RSMS.DataContext.EFCore;
+global using RSMS.Presenters;
+global using RSMS.Repositories;
